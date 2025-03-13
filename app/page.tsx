9a6708cbe8880f38bdabf63e3b772643d1b192f9
victor <oguntayovictor65@gmail.com>
2025-03-14 00:29:23 +0100

    work on navigating to different pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link";
+
 function page() {
   return (
-    <div>Home Page </div>
-  )
+    <div>
+      <h1 className="text-2xl"> Home Page</h1>
+      <Link href={"/about"} className="text-red-200 inline-block mt-8">
+        about page
+      </Link>
+    </div>
+  );
 }
-export default page
+export default page;
