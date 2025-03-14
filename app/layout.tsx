8eb018339142a2ea3ebc20014841b663bbb64ed9
victor <oguntayovictor65@gmail.com>
2025-03-14 16:20:34 +0100

    work on adding metadata in nextjs app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import { Inter, Inconsolata } from "next/font/google";
+import { title } from "process";
+import type { Metadata } from "next";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 const inconsolata = Inconsolata({ subsets: ["latin"] });
 
+export const metadata:Metadata = {
+  title: 'My Next.js App',
+  description: 'A next.js project with Typescript and Tailwind CSS',
+  keywords:'Next.js , typescript , tailwindcss'
+  
+}
 
 export default function RootLayout({
   children,
