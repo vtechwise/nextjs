5d0474251cce5bbfb48e13ac7da73213db92784c
victor <oguntayovictor65@gmail.com>
2025-03-17 04:26:26 +0100

    work on local image

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,7 +1,17 @@
+import image from "@/images/testing (2).jpg";
+import Image from "next/image";
+
 function page({ params }: { params: { id: string } }) {
   return (
     <div>
       <h2 className="text-2xl">ID :{params.id}</h2>
+      <section className="flex gap-x-4">
+        {/* local image */}
+        <div>
+          <Image src={image} alt="testing img" height={192} width={192} className="w-48 h-48 object-cover rounded" />
+          <h3>holiday in china</h3>
+        </div>
+      </section>
     </div>
   );
 }
