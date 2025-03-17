180708726c0650b8fa5429ec778a8ec7c59e9d0d
victor <oguntayovictor65@gmail.com>
2025-03-17 01:15:08 +0100

    render children in the nested layout

--- app/tours/layout.tsx
+++ app/tours/layout.tsx
@@ -6,6 +6,7 @@ function layout({children}:{children:React.ReactNode}) {
           <header className="rounded mb-4 py-2 w-1/2 bg-slate">
           <h1 className="text-3xl text-white text-center">Nested Layout</h1>
           </header>
+          <main>{children}</main>
     </div>
   )
 }
