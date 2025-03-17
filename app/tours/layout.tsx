383bb098645bf200d521c909f9195bc6c558f356
victor <oguntayovictor65@gmail.com>
2025-03-17 01:13:44 +0100

    work on nested layout

---
+++ app/tours/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function layout({children}:{children:React.ReactNode}) {
+  return (
+      <div>
+          <header className="rounded mb-4 py-2 w-1/2 bg-slate">
+          <h1 className="text-3xl text-white text-center">Nested Layout</h1>
+          </header>
+    </div>
+  )
+}
+export default layout
