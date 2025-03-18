6705359706b06b05a47475909d228836e6e30814
victor <oguntayovictor65@gmail.com>
2025-03-18 07:11:51 +0100

    work on creating action in rsc

---
+++ app/actions/page.tsx
@@ -0,0 +1,12 @@
+import Form from "@/components/Form"
+import UserLists from "@/components/UserLists"
+
+function ActionPage() {
+  return (
+      <div>
+          <Form />
+          <UserLists/>
+    </div>
+  )
+}
+export default ActionPage
