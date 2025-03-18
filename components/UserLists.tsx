6e033b6b498e9f3cecec3b15df0b94098a7563b6
victor <oguntayovictor65@gmail.com>
2025-03-18 23:26:08 +0100

    work on userList component

--- components/UserLists.tsx
+++ components/UserLists.tsx
@@ -1,6 +1,26 @@
-function UserLists() {
+import { fetchUsers } from "@/utils/actions";
+
+async function UserLists() {
+  const users = await fetchUsers();
   return (
-    <div>UserLists</div>
-  )
+    <div className="mt-4">
+      {users.length ? (
+        <div>
+          {users.map((user) => {
+            return (
+              <h4 className="text-xl capitalize" key={user.id}>
+                {user.firstName} {user.lastName}
+              </h4>
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          {" "}
+          <h2 className="texl-lg"> No user found</h2>
+        </div>
+      )}
+    </div>
+  );
 }
-export default UserLists
+export default UserLists;
