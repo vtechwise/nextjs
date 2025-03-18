c001abb8665277b4d193c36aa74d4865952347f3
victor <oguntayovictor65@gmail.com>
2025-03-19 00:48:52 +0100

    add deleteBtn component

---
+++ components/DeleteBtn.tsx
@@ -0,0 +1,6 @@
+function DeleteBtn({ id }: { id: string }) {
+  return (
+    <div className="bg-red-500 text-white text-sm p-2 rounded">Delete</div>
+  );
+}
+export default DeleteBtn;
--- components/UserLists.tsx
+++ components/UserLists.tsx
@@ -1,4 +1,5 @@
 import { fetchUsers } from "@/utils/actions";
+import DeleteBtn from "./DeleteBtn";
 
 async function UserLists() {
   const users = await fetchUsers();
@@ -8,8 +9,12 @@ async function UserLists() {
         <div>
           {users.map((user) => {
             return (
-              <h4 className="text-xl capitalize" key={user.id}>
+              <h4
+                className="text-xl capitalize flex justify-between items-center  mb-4"
+                key={user.id}
+              >
                 {user.firstName} {user.lastName}
+                <DeleteBtn id={user.id} />
               </h4>
             );
           })}
