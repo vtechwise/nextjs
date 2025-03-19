91583b41322d3f740c96318252cf85e555d49a98
victor <oguntayovictor65@gmail.com>
2025-03-20 00:29:44 +0100

    work on nextRequest and nextResponse in in users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,9 @@
 import { fetchUsers } from "@/utils/actions";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
+  console.log(req.nextUrl.searchParams.get("id"));
+
   const users = await fetchUsers();
-  return Response.json(users);
+  return NextResponse.redirect(new URL("/", req.url));
 };
